8741c7854c1597b46c39429df7e9096a698870a9
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-07-23 09:52:07 +0100

    Added environment variables

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Initialisation of the server
 
+require('dotenv').config() //npm install dotenv (in packaje.json)
+
 const app = require('./app')
-const port = 3000
+const port = process.env.PORT || 5001 // grabbing the hidden key PORT from .eng
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
