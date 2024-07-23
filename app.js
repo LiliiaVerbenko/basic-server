53a622fd4db8431b9ab40390fb17a8cf8f77e3c6
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-07-23 11:38:45 +0100

    Added cors

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const fruitRouter = require('./routes/fruitRouter')
+
+app.use(cors()) // npm install cors
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// CORS - Cors origin resource sharing
 
 app.use(express.json())
 app.use('/fruits', fruitRouter)
