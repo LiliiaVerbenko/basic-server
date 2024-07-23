e6b1a14d51114639bca441453d05762115e903cf
“LiliiaVerbenko” <verbenkoliliia@gmail.com>
2024-07-23 09:37:57 +0100

    My first commit

---
+++ controllers/fruits.js
@@ -0,0 +1,76 @@
+const Fruit = require('../models/Fruit')
+
+const index = async (req, res) => {
+    // showAll()
+    try{
+        const fruit =  await Fruit.showAll()
+        res.status(200).send(fruit)
+    } catch(error) {
+        res.status(404).send({error:err})
+    }
+    const fruits = Fruit.showAll()
+    res.status(200).send(fruits)
+  }
+
+const show = async (req, res) => {
+    const input = req.params.name.toLowerCase()
+
+    try {
+          const fruit = await Fruit.show(input)
+           res.status(200).send(fruit)
+        } catch (err) {
+             res.status(404).send({error: err})
+            }  
+
+
+}
+
+const create = async (req, res) => {
+    const data = req.body
+
+    try{
+        //1. Call a method from the model
+        const newFruit = await Fruit.create(data)
+
+        //2, Send a responce with a status code and the new element
+        res.status(201).send(newFruit) // 401 - conflift in creation, "fruit alreafy exists"
+    } catch(err) {
+        res.status(409).send({error: err})
+    }
+}
+
+const update = async (req,res) => {
+    const data = req.body;
+    const name = req.params.name.toLowerCase();
+
+    try{
+    // retrieving the spesific fruit that we want to update
+    const fruit = await Fruit.show(name)
+    const updatedFruit = await fruit.update(data)
+    res.status(200).send(updatedFruit)
+    } catch(err){
+        res.status(404).send({error: err})
+    }
+}
+
+const destroy = async (req, res) => {
+    const name = req.params.name.toLowerCase();
+
+    try {
+      const fruit = await Fruit.show(name);
+      const result = await fruit.destroy();
+
+      res.sendStatus(204)
+    } catch (err) {
+      res.status(404).send({ error: err});
+    }
+};
+
+
+//  module.exports = {index, show, create, update, destroy}
+
+module.exports = {index, show, create, update, destroy}
+
+
+
+// mc-backend-restful-complete
---
+++ routes/fruitRouter.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fruitRouter = express.Router();
+const fruits = require('../controllers/fruits')
+
+
+
+fruitRouter.get('/', fruits.index)
+fruitRouter.get('/:name', fruits.show)
+
+
+// Create - POST - create
+
+fruitRouter.post('/', fruits.create)
+
+// Update - PATCH - update
+
+
+fruitRouter.patch('/:name', fruits.update )
+
+// Delete - DELETE 
+
+fruitRouter.delete('/:name', fruits.destroy)
+
+module.exports = fruitRouter
